2edfcb713c202ea3f90795d00237999a454d6016
dennisdeveloper1989 <dennisdeveloper@icloud.com>
2022-11-29 10:14:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_TEST1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+
+            Console.ReadKey();
+        }
+    }
+}
